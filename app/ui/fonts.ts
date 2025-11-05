f2239c42281ec60cdfc9419832f87cfa1ec19a60
Layek Miah <layekofficial63@gmail.com>
2025-11-05 08:13:33 +0600

    fix a font bug

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Lusitana } from "next/font/google";
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"], // Specify desired weights
+  subsets: ["latin"], // Specify desired subsets
+  display: "swap", // Recommended for better user experience
+});
